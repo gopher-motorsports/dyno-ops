4b7600eda00e89316d168a680c915956d41b6162
Adrian Leal <adrianlealalonse@gmail.com>
2021-04-16 21:52:22 -0500

    demo code

--- data_entry/admin.py
+++ data_entry/admin.py
@@ -3,5 +3,4 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
-admin.site.register(Test)
-admin.site.register(Person)
+admin.site.register(Dyno)
--- data_entry/models.py
+++ data_entry/models.py
@@ -3,26 +3,11 @@ from django.contrib.postgres.fields import ArrayField
 from django.contrib.auth.models import User
 from django.utils.timezone import now
 
-
-class Person(models.Model):
-    email = models.CharField(max_length=100)
+class Dyno(models.Model):
     name = models.CharField(max_length=100)
-    subteam = models.CharField(max_length=100)
-    phone = models.CharField(max_length=100, default=000-000-0000)
-    licensed = models.BooleanField(default=False)
-
-class Test(models.Model):
-    weather = models.CharField(max_length=100)
-    temperature = models.IntegerField()
-    driver = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-    track = models.CharField(max_length=100, blank=True)
-    fast_lap = models.CharField(max_length=100, blank=True)
-    tires = models.CharField(max_length=100)
-    tire_condition = models.CharField(max_length=100)
-    engine = models.CharField(max_length=100, blank=True)
-    software = models.FloatField(blank=True)
-    comments = models.TextField(blank=True)
-    created_at = models.DateTimeField(default=now)
-    personnel = ArrayField(models.CharField(max_length=200), null=True)
+    date = models.DateTimeField(default=now)
+    test = models.CharField(max_length=100)
+    constant = models.BooleanField(default=True)
+    throttle = models.IntegerField()
+    rpm = models.IntegerField()
     
